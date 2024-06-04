ba1332da977ca85a5bc4fc37b9c8b37bc0e54209
rare1k <hi@rare1k.dev>
2024-06-04 16:23:03 -0400

    feat: start working on searchxng scraper

---
+++ modules/scraper.ts
@@ -0,0 +1,14 @@
+import cheerio from "cheerio";
+let ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+export async function scrapeSearXNG() {
+    let res = await (await fetch("https://searxng.site/searxng/search?q=site:porkbun.com+%22?coupon=%22", {
+        "headers": {
+            "User-Agent":ua
+        }
+    })).text()
+    let c = cheerio.load(res)
+    let lastPage = parseInt(c('input.page_number').last().val() as string);
+    for (let i = 1; i > lastPage; i++) {
+
+    }
+}
