66f4d76e09b0f4dea32580ed44031eb966eeae19
rare1k <hi@rare1k.dev>
2024-06-04 17:15:57 -0400

    feat: start working on recursively scraping links

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-console.log("Hello via Bun!");
+import { scrapeSearXNG } from "./modules/scraper";
+import logger from "./modules/logger";
+logger.info("Scraping Links!")
+scrapeSearXNG()
--- modules/scraper.ts
+++ modules/scraper.ts
@@ -1,15 +1,37 @@
 import cheerio from "cheerio";
-let ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+import logger from "./logger";
+let ua:string = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
 export async function scrapeSearXNG() {
-    let res = await (await fetch("https://searxng.site/searxng/search?q=site:porkbun.com+%22?coupon=%22", {
+    let links:string[] = []
+    let res = await (await fetch("https://searxng.ca/search?q=site:porkbun.com+%22?coupon=%22", {
         "headers": {
             "User-Agent":ua
         }
     })).text()
+    console.log(res)
     let c = cheerio.load(res)
-    let lastPage = parseInt(c('input.page_number').last().val() as string);
+    console.log(c('input.page_number').last().attr("value"))
+    let lastPage:number = parseInt(c('input.page_number').last().attr('value') as string);
+    logger.info(`🔍 Got ${lastPage} pages`)
     // "recursively" scrape for links
     for (let i = 1; i > lastPage; i++) {
-
+        let linkCount:number = 0;
+        logger.info(`🔍 Scraping for Porkbun Coupons (page ${i}/${lastPage})`)
+        let res = await (await fetch(`https://searxng.site/searxng/search?q=site:porkbun.com+%22?coupon=%22&pageno=${i}`, {
+            "headers": {
+                "User-Agent":ua
+            }
+        })).text()
+        let c = cheerio.load(res)
+        c('h3 a').each((_, element) => {
+            const href = c(element).attr('href');
+            if (href) {
+              links.push(href);linkCount++;
+            }
+          });
+        logger.info(`🔗 Got ${linkCount} on page ${i}`)
+        logger.info(`  Total: ${links.length}`)
     }
+    logger.info(`🔗 Got ${links.length} total!`)
+    return links
 }
